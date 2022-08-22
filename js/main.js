8199f9cfb6115c80d4d1c00a6a234acb38e73f3c
SalehZo-Niloy <msnniloy7@gmail.com>
2022-08-23 02:40:45 +0600

    player expense calculation done by JavaScript

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// player selection 
+document.getElementById('player-select').addEventListener('click', function (event) {
+    const players = document.getElementById('player-list').children.length;
+    if (players < 5) {
+        if (event.target.matches('.selected')) {
+            const player = document.createElement('li');
+            player.innerText = (event.target.previousSibling).previousSibling.innerText;
+            document.getElementById('player-list').appendChild(player);
+            event.target.setAttribute('disabled', true);
+            event.target.style.backgroundColor = 'grey';
+        }
+    }
+    else {
+        alert('Can not add more than five players');
+    }
+})
+// get input field value
+function getInputFieldValue(elementId) {
+    const inputField = document.getElementById(elementId);
+    const inputFieldString = inputField.value;
+    const inputFieldNumber = Number(inputFieldString);
+
+    return inputFieldNumber;
+}
+// set element value
+function setElementValue(elementId, value) {
+    const element = document.getElementById(elementId);
+    if (isNaN(value)) {
+        element.innerText = 0;
+        return;
+    }
+    element.innerText = value;
+}
+// budget per player calculation
+document.getElementById('calculate').addEventListener('click', function () {
+    const perPlayerCost = getInputFieldValue('per-player-cost');
+    if (isNaN(perPlayerCost) || perPlayerCost === 0) {
+        alert('Invalid Input');
+    }
+    const playerNumber = document.getElementById('player-list').children.length;
+    if (playerNumber === 0) {
+        alert('Select players');
+        return;
+    }
+    const playerExpense = perPlayerCost * playerNumber;
+
+    setElementValue('player-expenses', playerExpense);
+
+})
